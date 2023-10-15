d80d93b936b100ffb1f61a3ef8100fa5dedb327f
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2023-10-15 21:22:16 +0800

    Delete semilearn/algorithms/SemiReward directory

--- semilearn/algorithms/SemiReward/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-from .main import Rewarder,Generator,cosine_similarity_n
-from .SemiRewardH import SemiReward_infer,SemiReward_train
